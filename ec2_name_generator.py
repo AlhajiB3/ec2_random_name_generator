dc0297b69b0f801b5c948fb1f2a7e2ad1a551854
AlhajiB3 <133896864+AlhajiB3@users.noreply.github.com>
2024-07-29 15:12:25 -0400

    ec2-name-generator072924

---
+++ ec2_name_generator.py
@@ -0,0 +1,11 @@
+import uuid 
+
+# This will import and generate a unique ideniftier for our unique name
+
+for _ in range(int(input('How many EC2 instances would you like unique names for? '))):
+
+# This will prompt the user for the # of EC2 instances in the "range" function
+
+    print(f"{input('What is the name of your department? ')}-{uuid.uuid4()}")
+
+# Each instance will have user for the department name and will print the unique EC2 name based on the format "department-uuid"
